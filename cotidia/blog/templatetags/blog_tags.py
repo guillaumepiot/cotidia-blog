5c58e339f6b1bbcd87ddfdfe7870e3dae1f014ad
Guillaume Piot <guillaume@cotidia.com>
2016-12-15 14:28:47 +0000

    Add blog tags: - Get latest article - Get next article - Get previous article

--- cotidia/blog/templatetags/blog_tags.py
+++ cotidia/blog/templatetags/blog_tags.py
@@ -14,8 +14,12 @@ def blog_nav():
 
 
 @register.inclusion_tag('blog/includes/_archive.html', takes_context=True)
-def blog_archive(context, show_empty=blog_settings.ARCHIVE_SHOW_EMPTY, show_articles=blog_settings.ARCHIVE_SHOW_ARTICLES, show_count=blog_settings.ARCHIVE_SHOW_COUNT):
-    archive = {}
+def blog_archive(
+        context,
+        show_empty=blog_settings.ARCHIVE_SHOW_EMPTY,
+        show_articles=blog_settings.ARCHIVE_SHOW_ARTICLES,
+        show_count=blog_settings.ARCHIVE_SHOW_COUNT
+        ):
     year_list = []
     articles = Article.objects.get_published_live().order_by('-publish_date')
     request = context['request']
@@ -33,19 +37,53 @@ def blog_archive(context, show_empty=blog_settings.ARCHIVE_SHOW_EMPTY, show_arti
         for i in range(year_range+1):
             year_list.append(Year(latest_year - i))
 
-    return {'archive': year_list, 'request': request, 'show_empty': show_empty, 'show_articles': show_articles, 'show_count': show_count }
+    return {
+        'archive': year_list,
+        'request': request,
+        'show_empty': show_empty,
+        'show_articles': show_articles,
+        'show_count': show_count
+        }
+
+
+@register.assignment_tag
+def get_latest_article():
+    return Article.objects.get_published_live().latest('publish_date')
+
+
+@register.assignment_tag
+def get_next_article(current_article):
+    queryset = Article.objects.get_published_live().filter(
+        publish_date__gte=current_article.publish_date).exclude(
+        id=current_article.id).order_by('publish_date')
+    if queryset.count():
+        return queryset.first()
+    else:
+        return None
+
+
+@register.assignment_tag
+def get_previous_article(current_article):
+    queryset = Article.objects.get_published_live().filter(
+        publish_date__lte=current_article.publish_date).exclude(
+        id=current_article.id)
+    if queryset.count():
+        return queryset.latest('publish_date')
+    else:
+        return None
 
 
 @register.assignment_tag
 def get_latest_articles(limit=3):
-    articles = Article.objects.get_published_live().order_by('-publish_date')[:int(limit)]
-    return articles
+    return Article.objects.get_published_live().order_by(
+        '-publish_date')[:int(limit)]
 
 
 @register.assignment_tag
 def get_latest_articles_per_language(language, limit=False):
     from django.utils.translation import get_language
-    articles = Article.objects.get_published_translation_live(language_code=get_language()).order_by('-parent__publish_date')
+    articles = Article.objects.get_published_translation_live(
+        language_code=get_language()).order_by('-parent__publish_date')
     if limit:
         articles = articles[:int(limit)]
     return articles
@@ -53,8 +91,13 @@ def get_latest_articles_per_language(language, limit=False):
 
 @register.assignment_tag
 def get_latest_by_author(author, limit=3, exclude=False):
+
+    queryset = Article.objects.get_published_live().filter(
+        published_from__authors=author)
+
     if exclude:
-        articles = Article.objects.get_published_live().filter(published_from__authors=author).exclude(id=exclude.id).order_by('-publish_date')[:int(limit)]
+        return queryset.exclude(
+            id=exclude.id).order_by('-publish_date')[:int(limit)]
     else:
-        articles = Article.objects.get_published_live().filter(published_from__authors=author).order_by('-publish_date')[:int(limit)]
-    return articles
+        return queryset.order_by(
+            '-publish_date')[:int(limit)]
